47b79e08e26e9f54dac721a492b3afaca3a377eb
Mike Dean <miketraindoc@gmail.com>
2024-09-18 16:17:42 -0600

    App runs on localhost.  Now to HF!

--- Midterm/app.py
+++ Midterm/app.py
@@ -109,9 +109,26 @@ print(page_response)
 
 @cl.on_chat_start
 async def on_chat_start():
-    # Create a message and send it to the user
-    msg = cl.Message(content="Ask me questions about these topics!")
-    await msg.send()  # Send the message first
+    welcome_message = f"""
+# 🪙 Welcome to our Company's Responsible AI Development Question and Answer Chatbot!
+
+As you know, AI is a hot topic, and our company is engaged in developing software 
+applications.  We believe that we will inevitably be developing software that utilizes
+AI, but people are rightfully concerned about the implications of using AI.  Nobody seems to understand
+the riht way to think about building ethical and useful AI applications for enterprises.  
+
+I am here to help you understand how the AI industry is evolving, especially as it relates to politics.
+Many people believe that the best guidance is likely to come from the government, so I have thoroughly
+read two important documents:
+
+1.  White House Blueprint for an AI Bill of Rights, Making Automated Systems Work for the American People, October 2022
+2.  Artificial Intelligence Risk Management Framework: Generative Artificial Intelligence Profile (NIST AI 600-1)
+
+Ask me questions about AI, its implications, where the industry is heading, etc.  I will try to help!
+
+"""
+    
+    await cl.Message(content=welcome_message).send()
 
     # Set the chain in the session
     cl.user_session.set("chain", page_split_rag_chain)
